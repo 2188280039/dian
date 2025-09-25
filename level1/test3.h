49bb499124555259990b93e76bca3abde8781397
九思 <62779520+2188280039@users.noreply.github.com>
2025-09-25 16:05:07 +0800

    Add files via upload

---
+++ level1/test3.h
@@ -0,0 +1,8 @@
+#include "test3funtion.c"
+
+char Loginf();//登录函数
+int decide();//判定操作函数
+int search();//查询函数
+int reserve(char operation[],char userf);//预约函数
+int check(char User);//查询预约函数
+int clear(char User);//清除函数
