4efbb59d4a0990a0403bf98c9318716d2840decc
九思 <62779520+2188280039@users.noreply.github.com>
2025-09-29 13:03:18 +0800

    Delete level1 directory

--- level1/test3.h
+++
@@ -1,8 +0,0 @@
-#include "test3funtion.c"
-
-char Loginf();//登录函数
-int decide();//判定操作函数
-int search();//查询函数
-int reserve(char operation[],char userf);//预约函数
-int check(char User);//查询预约函数
-int clear(char User);//清除函数
